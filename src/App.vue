eb0ced748e7b0d05637148869729748601559b16
LiHongyan11 <179947177@qq.com>
2020-12-29 21:32:00 +0800

    feat: update publish info

--- src/App.vue
+++ src/App.vue
@@ -23,23 +23,29 @@
     </div>
 </template>
 
-<script lang="ts">
+<script>
 
-import { Component, Vue, Watch } from 'vue-property-decorator';
-import Tree from './components/tree.vue';
+import Tree from 'vue-infinite-tree';
+import 'vue-infinite-tree/dist/vue-infinite-tree.css';
 
-@Component({
+export default {
     name: 'case-1',
     components: {
-        Tree,
+      Tree,
     },
-})
-export default class Case1 extends Vue {
-    public text = '';
+    data() {
+      return {
+        text: '',
+        treeList: []
+      }
+    },
+    watch: {
+      text(val) {
+        this.$refs.tree.filter(val);
+      }
+    },
 
-    public treeList: any[] = [];
-
-    // public treeList: any[] = [{
+    // treeList: any[] = [{
     //     name: '一级 1',
     //     id: 1,
     // }, {
@@ -49,12 +55,8 @@ export default class Case1 extends Vue {
     //     id: 9,
     //     name: '一级 3',
     // }]
-    @Watch('text')
-    public onTextChange(val: string) {
-        this.$refs.tree.filter(val);
-    }
 
-    public mounted() {
+    mounted() {
         setTimeout(() => {
             this.treeList = [{
                 name: '一级 1',
@@ -90,85 +92,87 @@ export default class Case1 extends Vue {
                 }],
             }];
         }, 2000);
-    }
+    },
 
-    public renderContent(h: any, { node, data, store }: any) {
-        return h('span', {}, data.name);
-    }
+    methods: {
+      renderContent(h, { node, data, store }) {
+          return h('span', {}, data.name);
+      },
 
-    public load(parentNode: any, resolve: any) {
-        const nodes = [
-            {
-                id: 40,
-                name: 'Node 1',
-                isLeaf: true,
-            },
-            {
-                id: 50,
-                name: 'Node 2',
-                isLeaf: true,
-            },
-        ];
-        setTimeout(() => {
-            resolve([]);
-        }, 100);
-    }
+      load(parentNode, resolve) {
+          const nodes = [
+              {
+                  id: 40,
+                  name: 'Node 1',
+                  isLeaf: true,
+              },
+              {
+                  id: 50,
+                  name: 'Node 2',
+                  isLeaf: true,
+              },
+          ];
+          setTimeout(() => {
+              resolve([]);
+          }, 100);
+      },
 
-    public filterNode(value: any, data: any) {
-        if (!value) return true;
-        return data.name.indexOf(value) !== -1;
-    }
+      filterNode(value, data) {
+          if (!value) return true;
+          return data.name.indexOf(value) !== -1;
+      },
 
-    public handleClick() {
-        // this.updateKeyChildren();
-        // this.getCurrentKey();
-        // this.setCurrentKey();
-        // this.remove();
-        // this.append();
-        // this.insertBefore();
-        this.insertAfter();
-    }
+      handleClick() {
+          // this.updateKeyChildren();
+          // this.getCurrentKey();
+          // this.setCurrentKey();
+          // this.remove();
+          // this.append();
+          // this.insertBefore();
+          this.insertAfter();
+      },
 
-    public insertAfter() {
-        // this.$refs.tree.insertAfter({
-        //     id: 15,
-        //     name: 'test2',
-        // }, 12);
-    }
+      insertAfter() {
+          // this.$refs.tree.insertAfter({
+          //     id: 15,
+          //     name: 'test2',
+          // }, 12);
+      },
 
-    public insertBefore() {
-        // this.$refs.tree.insertBefore({
-        //     id: 15,
-        //     name: 'test2',
-        // }, 3);
-    }
+      insertBefore() {
+          // this.$refs.tree.insertBefore({
+          //     id: 15,
+          //     name: 'test2',
+          // }, 3);
+      },
 
-    public append() {
-        // this.$refs.tree.append({
-        //     id: 14,
-        //     name: 'test',
-        // }, 12);
-    }
+      append() {
+          // this.$refs.tree.append({
+          //     id: 14,
+          //     name: 'test',
+          // }, 12);
+      },
 
 
-    public remove() {
-        // this.$refs.tree.remove(12);
-    }
+      remove() {
+          // this.$refs.tree.remove(12);
+      },
 
-    public setCurrentKey() {
-        // this.$refs.tree.setCurrentKey(9);
-    }
+      setCurrentKey() {
+          // this.$refs.tree.setCurrentKey(9);
+      },
 
-    public getCurrentKey() {
-        // console.log(this.$refs.tree.getCurrentKey());
-        // console.log(this.$refs.tree.getCurrentNode());
-    }
+      getCurrentKey() {
+          // console.log(this.$refs.tree.getCurrentKey());
+          // console.log(this.$refs.tree.getCurrentNode());
+      },
 
-    public updateKeyChildren() {
-        // this.$refs.tree.updateKeyChildren(1, [{
-        //     id: 3,
-        //     name: '三级 1-1-1',
-        // }]);
+      updateKeyChildren() {
+          // this.$refs.tree.updateKeyChildren(1, [{
+          //     id: 3,
+          //     name: '三级 1-1-1',
+          // }]);
+      }
     }
 }
 </script>
